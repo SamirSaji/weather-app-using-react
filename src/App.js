16a5476f104097abec0b131f4ea0fe1db04618e5
SamirSaji <samirsaji13@gmail.com>
2020-10-27 15:38:07 +0530

    complete search and show result overall completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
+import { Grid } from "@material-ui/core/";
+import LocationSearchInput from "./component/userlocation";
+function App(props) {
+  const [initial_location, update_location] = useState({
+    lat: "",
+    lng: "",
+  });
 
-function App() {
-  const [initial_data, update_data] = useState({});
+  const [initial_data, update_data] = useState({
+    description: "",
+    temperature: "",
+    location: "",
+    country: "",
+    wind_speed: "",
+    humidity: "",
+    pressure: "",
+  });
+
+  const onLocation = (data) => {
+    let locationData = {
+      lat: data.lat,
+      lng: data.lng,
+    };
+
+    update_location(locationData);
+  };
 
   useEffect(() => {
-    async function apifunc() {
+    async function apifunc(props) {
       await axios
         .get(
-          "https://api.openweathermap.org/data/2.5/weather?lat=11.229592&lon=78.171158&appid=7854e53a58de1d8bcc4635736c7b63d5"
+          `https://api.openweathermap.org/data/2.5/weather?lat=${initial_location.lat}&lon=${initial_location.lng}&appid=7854e53a58de1d8bcc4635736c7b63d5`
         )
         .then((response) => {
           let climatedata = {
@@ -28,23 +51,75 @@ function App() {
           console.log(error);
         });
     }
-    apifunc();
-  }, []);
+    if (initial_location.lat !== "") {
+      apifunc();
+    }
+  }, [initial_location]);
 
   return (
-    <div className="App">
-      <div className="head_container">
-        <p>{initial_data.location}</p>
-        <p>{initial_data.country}</p>
-        <p>{initial_data.description}</p>
-        <p>{initial_data.wind_speed}</p>
-        <p>{initial_data.temperature}</p>
-        <p>{initial_data.humidity}</p>
-        <p>{initial_data.pressure}</p>
-      </div>
+    <div className="App ">
+      <Grid className="head_container" container>
+        {initial_location.lat !== "" ? (
+          <Grid item xs={12} className="main_grid">
+            {initial_data.description === "" ? (
+              ""
+            ) : (
+              <>
+                <p className="description_text">{`${
+                  initial_data.description
+                }  ( ${Math.round(initial_data.temperature - 273.15)}°C ) `}</p>
+                <p className="country_text">{`${initial_data.location}, ${initial_data.country}`}</p>
+              </>
+            )}
+
+            <Grid className="content_grid">
+              {initial_data.wind_speed === "" ? (
+                ""
+              ) : (
+                <>
+                  {" "}
+                  <Grid item xs={4}>
+                    <p className="extra_content">{initial_data.wind_speed}</p>
+                    <p className="extra_heading">Wind Speed</p>{" "}
+                  </Grid>
+                </>
+              )}
+
+              {initial_data.humidity === "" ? (
+                ""
+              ) : (
+                <>
+                  {" "}
+                  <Grid item xs={4}>
+                    <p className="extra_content">{initial_data.humidity}</p>
+                    <p className="extra_heading">Humidity</p>
+                  </Grid>
+                </>
+              )}
+
+              {initial_data.pressure === "" ? (
+                ""
+              ) : (
+                <>
+                  {" "}
+                  <Grid item xs={4}>
+                    <p className="extra_content">{initial_data.pressure}</p>
+                    <p className="extra_heading">Pressure</p>{" "}
+                  </Grid>
+                </>
+              )}
+            </Grid>
+          </Grid>
+        ) : (
+          ""
+        )}
+
+        <Grid item xs={10} sm={8} md={4} className="searchbar_grid">
+          <LocationSearchInput onLocation={onLocation} />
+        </Grid>
+      </Grid>
     </div>
   );
 }
 
 export default App;
-
---
+++ src/component/userlocation.js
@@ -0,0 +1,142 @@
+import React from "react";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
+import { TextField } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+import "../App.css";
+// import Weather from "./weather";
+const styles = (theme) => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  textField: {
+    marginLeft: `theme.spacing.unit * y`,
+    marginRight: `theme.spacing.unit * y`,
+    width: "100%",
+  },
+
+  cssLabel: {
+    color: "#00000 !important",
+  },
+
+  cssOutlinedInput: {
+    "&$cssFocused $notchedOutline": {
+      borderColor: "#102D46 !important",
+    },
+  },
+
+  cssFocused: {
+    color: "#000 !important",
+  },
+
+  notchedOutline: {
+    borderWidth: "1px",
+    borderColor: "#102D46 !important",
+  },
+});
+
+class LocationSearchInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { address: "" };
+    // this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  handleChange = (address) => {
+    this.setState({ address });
+  };
+
+  handleSelect = (address) => {
+    geocodeByAddress(address)
+      .then((results) => getLatLng(results[0]))
+      .then((latLng) => {
+        if (this.props.onLocation) {
+          this.props.onLocation(latLng);
+        }
+      })
+      .catch((error) => console.error("Error", error));
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <PlacesAutocomplete
+        value={this.state.address}
+        onChange={this.handleChange}
+        onSelect={this.handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <TextField
+              InputLabelProps={{
+                classes: {
+                  root: classes.cssLabel,
+                  focused: classes.cssFocused,
+                },
+              }}
+              InputProps={{
+                classes: {
+                  root: classes.cssOutlinedInput,
+                  focused: classes.cssFocused,
+                  notchedOutline: classes.notchedOutline,
+                },
+                inputMode: "numeric",
+              }}
+              {...getInputProps({
+                placeholder: "Search Places ...",
+                className: "location-search-input",
+              })}
+              id="outlined-basic"
+              label="Search Location"
+              variant="outlined"
+              size="small"
+              autoFocus
+              style={{ width: "100%" }}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map((suggestion) => {
+                const className = suggestion.active
+                  ? "suggestion-item--active"
+                  : "suggestion-item";
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? {
+                      backgroundColor: "tansparent",
+                      cursor: "pointer",
+                      // opacity: "0.5",
+                    }
+                  : {
+                      backgroundColor: "transparent",
+                      cursor: "pointer",
+                      opacity: "0.7",
+                    };
+                return (
+                  <div
+                   
+                    className="dropdown_class"
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span  >{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+    );
+  }
+}
+LocationSearchInput.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(LocationSearchInput);
